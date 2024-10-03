e031e1a70b08873b75b47cdedca485dee635531c
David Salazar G <david.1701713821@ucaldas.edu.co>
2024-10-03 15:29:27 -0500

    aplicacion de carrito de compras video 46

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,41 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Teclado mecánico ASUS RGB TUF',
+        description: 'Teclado mecánico con iluminación RGB y switches Cherry MX Red, ideal para gaming.',
+        price: 1200
+    },
+    {
+        id: 2,
+        name: 'Teclado Corsair K95 RGB Platinum',
+        description: 'Teclado mecánico premium con retroiluminación RGB y switches Cherry MX Speed.',
+        price: 1500
+    },
+    {
+        id: 3,
+        name: 'Teclado Logitech G Pro X',
+        description: 'Teclado compacto para eSports, con switches intercambiables y retroiluminación RGB.',
+        price: 1300
+    },
+    {
+        id: 4,
+        name: 'Razer Huntsman Elite',
+        description: 'Teclado con switches opto-mecánicos y retroiluminación RGB, con reposamuñecas ergonómico.',
+        price: 1400
+    },
+    {
+        id: 5,
+        name: 'SteelSeries Apex Pro',
+        description: 'Teclado mecánico con switches ajustables y tecnología OLED Smart Display.',
+        price: 1600
+    },
+    {
+        id: 6,
+        name: 'HyperX Alloy FPS Pro',
+        description: 'Teclado mecánico compacto con retroiluminación roja y switches Cherry MX Red.',
+        price: 1100
+    }
+
+]
