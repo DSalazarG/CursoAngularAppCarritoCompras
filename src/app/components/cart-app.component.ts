fde65515c00a4461e7924544c423b3d1d1461f46
David Salazar G <david.1701713821@ucaldas.edu.co>
2024-10-03 23:39:34 -0500

    previo al video 54, respaldo

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -4,11 +4,12 @@ import { Product } from '../models/product';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
 import { CartItem } from '../models/cartItem';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent, CartComponent],
+  imports: [CatalogComponent, CartComponent, NavbarComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit{
@@ -17,13 +18,56 @@ export class CartAppComponent implements OnInit{
 
   items: CartItem[] = []; //productos o items añadidos al carrito de compras
 
+  total: number = 0; //total de todos los productos en el carrito de compras
+
+  showCart: boolean = false;
+
   constructor(private service: ProductService){}
 
-  onAddCartMain(product: Product){
-    this.items = [... this.items, {product: {... product}, quantity: 1}]
+  onAddCartMain(product: Product): void{
+    const hasItem = this.items.find(item => {
+      return item.product.id === product.id;
+    })
+
+    if(hasItem){
+      this.items = this.items.map(item =>{
+        if(item.product.id === product.id){
+            return {... item, quantity: item.quantity + 1}
+        } 
+        return item
+      })
+    }else{
+      this.items = [... this.items, {product: {... product}, quantity: 1}];
+    }
+    this.calculateTotal();
+    this.saveSession();
+  }
+
+  onDeleteCartMain(id: number): void{
+    this.items = this.items.filter(item => item.product.id != id);
+    this.calculateTotal();
+    this.saveSession();
+
+  }
+
+  calculateTotal(): void{
+    this.total = 0;
+    this.items.forEach(item => {
+      this.total += item.product.price * item.quantity;
+    });
+  }
+
+  saveSession(): void{
+    sessionStorage.setItem('cart', JSON.stringify(this.items))
+  }
+
+  openCart(): void{
+    this.showCart = !this.showCart;
   }
 
   ngOnInit(): void {
     this.products = this.service.findAll();
+    this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
+    this.calculateTotal();
   }
 }
